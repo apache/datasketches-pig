b38fb7b9b4bfda10a3393b4f227842b108838274
saydakov <saydakov@yahoo-inc.com>
2016-03-09 14:34:45 -0800

    DoubleSummarySketchToEstimates

---
+++ src/main/java/com/yahoo/sketches/pig/tuple/DoubleSummarySketchToEstimates.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+package com.yahoo.sketches.pig.tuple;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+
+import com.yahoo.sketches.memory.NativeMemory;
+import com.yahoo.sketches.tuple.DoubleSummary;
+import com.yahoo.sketches.tuple.Sketch;
+import com.yahoo.sketches.tuple.SketchIterator;
+import com.yahoo.sketches.tuple.Sketches;
+
+/**
+ * This UDF converts a Sketch<DoubleSummary> to estimates.
+ * The first estimate is the estimate of the number of unique
+ * keys in the original population.
+ * The second is the estimate of the sum of the parameter
+ * in the original population (sums of the values in the sketch
+ * scaled to the original population). This estimate assumes
+ * that the DoubleSummary was used in the Sum mode.
+ */
+public class DoubleSummarySketchToEstimates extends EvalFunc<Tuple> {
+  @Override
+  public Tuple exec(Tuple input) throws IOException {
+    if ((input == null) || (input.size() == 0)) {
+      return null;
+    }
+
+    DataByteArray dba = (DataByteArray) input.get(0);
+    Sketch<DoubleSummary> sketch = Sketches.heapifySketch(new NativeMemory(dba.get()));
+
+    Tuple output = TupleFactory.getInstance().newTuple(2);
+    output.set(0, sketch.getEstimate());
+    double sum = 0;
+    SketchIterator<DoubleSummary> it = sketch.iterator();
+    while (it.next()) {
+      sum += it.getSummary().getValue();
+    }
+    output.set(1, sum / sketch.getTheta());
+
+    return output;
+  }
+}
---
+++ src/test/java/com/yahoo/sketches/pig/tuple/DoubleSummarySketchToEstimatesTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+package com.yahoo.sketches.pig.tuple;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.Tuple;
+
+import com.yahoo.sketches.tuple.UpdatableSketch;
+import com.yahoo.sketches.tuple.DoubleSummary;
+import com.yahoo.sketches.tuple.DoubleSummaryFactory;
+import com.yahoo.sketches.tuple.UpdatableSketchBuilder;
+
+public class DoubleSummarySketchToEstimatesTest {
+  @Test
+  public void emptySketch() throws Exception {
+    EvalFunc<Tuple> func = new DoubleSummarySketchToEstimates();
+    UpdatableSketch<Double, DoubleSummary> sketch = new UpdatableSketchBuilder<Double, DoubleSummary>(new DoubleSummaryFactory()).build();
+    Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketch.compact().toByteArray()));
+    Tuple resultTuple = func.exec(inputTuple);
+    Assert.assertNotNull(resultTuple);
+    Assert.assertEquals(resultTuple.size(), 2);
+    Assert.assertEquals(resultTuple.get(0), 0.0);
+    Assert.assertEquals(resultTuple.get(0), 0.0);
+  }
+
+  @Test
+  public void normalCase() throws Exception {
+    EvalFunc<Tuple> func = new DoubleSummarySketchToEstimates();
+    UpdatableSketch<Double, DoubleSummary> sketch = new UpdatableSketchBuilder<Double, DoubleSummary>(new DoubleSummaryFactory()).build();
+    int iterations = 100000;
+    for (int i = 0; i < iterations; i++) sketch.update(i, 1.0);
+    for (int i = 0; i < iterations; i++) sketch.update(i, 1.0);
+    Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketch.compact().toByteArray()));
+    Tuple resultTuple = func.exec(inputTuple);
+    Assert.assertNotNull(resultTuple);
+    Assert.assertEquals(resultTuple.size(), 2);
+    Assert.assertEquals((double) resultTuple.get(0), iterations, iterations * 0.03);
+    Assert.assertEquals((double) resultTuple.get(1), 2 * iterations, 2 * iterations * 0.03);
+  }
+}
