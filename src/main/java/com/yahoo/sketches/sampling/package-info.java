2dc8b6e329b83ab360bdc177a6b12c0f190f9899
lrhodes <lrhodes@yahoo-inc.com>
2017-03-15 23:24:17 -0700

    Adding package-info file

---
+++ src/main/java/com/yahoo/sketches/sampling/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2017, Yahoo! Inc. Licensed under the terms of the Apache License 2.0. See LICENSE file
+ * at the project root for terms.
+ */
+/**
+ * The sampling package
+ */
+package com.yahoo.sketches.sampling;
