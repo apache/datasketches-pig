1abb450f6c700e69cd2f1fe9c307e2fdb8a63fa0
Josh Walters <josh@joshwalters.com>
2017-08-02 18:08:31 -0700

    Added p-value tuple UDF

---
+++ src/main/java/com/yahoo/sketches/pig/tuple/ArrayOfDoublesSketchesToPValueEstimates.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2016, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.sketches.pig.tuple;
+
+import com.yahoo.memory.Memory;
+import com.yahoo.sketches.tuple.ArrayOfDoublesSketch;
+import com.yahoo.sketches.tuple.ArrayOfDoublesSketches;
+
+import java.io.IOException;
+
+import org.apache.commons.math3.stat.inference.TTest;
+import org.apache.commons.math3.stat.StatUtils;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.Tuple;
+
+/**
+ * Calculate p-values given two ArrayOfDoublesSketch. Each value in the sketch
+ * is treated as a separate metric measurement, and a p-value will be generated
+ * for each metric.
+ *
+ * The t-statistic is calculated as follows: t = (m1 - m2) / sqrt(var1/n1 + var2/n2)
+ *
+ * Degrees of freedom is approximated by Welch-Satterthwaite.
+ */
+public class ArrayOfDoublesSketchesToPValueEstimates extends EvalFunc<Tuple> {
+    @Override
+    public Tuple exec(final Tuple input) throws IOException {
+        if ((input == null) || (input.size() != 2)) {
+            return null;
+        }
+
+        // Get the two sketches
+        final DataByteArray dbaA = (DataByteArray) input.get(0);
+        final DataByteArray dbaB = (DataByteArray) input.get(1);
+        final ArrayOfDoublesSketch sketchA = ArrayOfDoublesSketches.wrapSketch(Memory.wrap(dbaA.get()));
+        final ArrayOfDoublesSketch sketchB = ArrayOfDoublesSketches.wrapSketch(Memory.wrap(dbaB.get()));
+
+        // Check that the size of the arrays in the sketches are the same
+        if (sketchA.getNumValues() != sketchB.getNumValues()) {
+            throw new IllegalArgumentException("Both sketches must have the same number of values");
+        }
+
+        // Check if either sketch is empty
+        if (sketchA.isEmpty() || sketchB.isEmpty()) {
+            return null;
+        }
+
+        // Check that each sketch has at least 2 values
+        if (sketchA.getRetainedEntries() < 2 || sketchB.getRetainedEntries() < 2) {
+            return null;
+        }
+
+        // Get the values from each sketch
+        double[][] valuesA = sketchA.getValues();
+        double[][] valuesB = sketchB.getValues();
+        valuesA = rotateMatrix(valuesA);
+        valuesB = rotateMatrix(valuesB);
+
+        // Calculate the p-values
+        double[] pValues = new double[valuesA.length];
+        TTestWithSketch tTest = new TTestWithSketch();
+        for (int i = 0; i < valuesA.length; i++) {
+            // Do some special math to get an accurate mean from the sketches
+            // Take the sum of the values, divide by theta, then divide by the
+            // estimate number of records. Note, this only counts the number
+            // of distinct records, not the total number of records.
+            double meanA = (StatUtils.sum(valuesA[i]) / sketchA.getTheta()) / sketchA.getEstimate();
+            double meanB = (StatUtils.sum(valuesB[i]) / sketchB.getTheta()) / sketchB.getEstimate();
+            // Variance is based only on the samples we have in the tuple sketch
+            double varianceA = StatUtils.variance(valuesA[i]);
+            double varianceB = StatUtils.variance(valuesB[i]);
+            // Use the estimated number of uniques
+            // Not completely accurate, as repeats that share the same ID won't be in the estimate
+            double numA = sketchA.getEstimate();
+            double numB = sketchB.getEstimate();
+            pValues[i] = tTest.callTTest(meanA, meanB, varianceA, varianceB, numA, numB);
+        }
+
+        return Util.doubleArrayToTuple(pValues);
+    }
+
+    /**
+     * Perform a clockwise rotation the output values from the tuple sketch.
+     *
+     * @param m Input matrix to rotate
+     * @return Rotated matrix
+     */
+    private static double[][] rotateMatrix(double[][] m) {
+        final int width = m.length;
+        final int height = m[0].length;
+        double[][] result = new double[height][width];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                result[j][width - 1 - i] = m[i][j];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * This class exists to get around the protected tTest method.
+     */
+    private class TTestWithSketch extends TTest {
+        /**
+         * Call the protected tTest method.
+         */
+        public double callTTest(double m1, double m2, double v1, double v2, double n1, double n2) {
+            return super.tTest(m1, m2, v1, v2, n1, n2);
+        }
+    }
+}
---
+++ src/test/java/com/yahoo/sketches/pig/tuple/ArrayOfDoublesSketchesToPValueEstimatesTest.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright 2016, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.sketches.pig.tuple;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+
+import com.yahoo.sketches.tuple.ArrayOfDoublesUpdatableSketch;
+import com.yahoo.sketches.tuple.ArrayOfDoublesUpdatableSketchBuilder;
+
+import org.apache.commons.math3.stat.inference.TTest;
+import org.apache.commons.math3.stat.StatUtils;
+
+/**
+ * Test p-value estimation of two ArrayOfDoublesSketch.
+ */
+public class ArrayOfDoublesSketchesToPValueEstimatesTest {
+    /**
+     * Check null input to UDF.
+     */
+    @Test
+    public void nullInput() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        Tuple resultTuple = func.exec(null);
+
+        Assert.assertNull(resultTuple);
+    }
+
+    /**
+     * Check input of empty tuple.
+     */
+    @Test
+    public void emptyInput() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        Tuple resultTuple = func.exec(TupleFactory.getInstance().newTuple());
+
+        Assert.assertNull(resultTuple);
+    }
+
+    /**
+     * Check input of single empty sketch.
+     */
+    @Test
+    public void oneEmptySketch() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        ArrayOfDoublesUpdatableSketch sketch = new ArrayOfDoublesUpdatableSketchBuilder().build();
+
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketch.compact().toByteArray()));
+
+        Tuple resultTuple = func.exec(inputTuple);
+
+        Assert.assertNull(resultTuple);
+    }
+
+    /**
+     * Check input of two empty sketches.
+     */
+    @Test
+    public void twoEmptySketches() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        ArrayOfDoublesUpdatableSketch sketchA = new ArrayOfDoublesUpdatableSketchBuilder().build();
+        ArrayOfDoublesUpdatableSketch sketchB = new ArrayOfDoublesUpdatableSketchBuilder().build();
+
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketchA.compact().toByteArray()),
+                                                  new DataByteArray(sketchB.compact().toByteArray()));
+
+        Tuple resultTuple = func.exec(inputTuple);
+
+        Assert.assertNull(resultTuple);
+    }
+
+    /**
+     * Check p-value for the smoker data set. Single metric.
+     */
+    @Test
+    public void smokerDatasetSingleMetric() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        // Create the two sketches
+        ArrayOfDoublesUpdatableSketch sketchA = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(1)
+                                                    .build();
+        ArrayOfDoublesUpdatableSketch sketchB = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(1)
+                                                    .build();
+
+        // Sample dataset (smoker/non-smoker brain size)
+        double[] groupA = {7.3, 6.5, 5.2, 6.3, 7.0, 5.9, 5.2, 5.0, 4.7, 5.7, 5.7, 3.3, 5.0, 4.6, 4.8, 3.8, 4.6};
+        double[] groupB = {4.2, 4.0, 2.6, 4.9, 4.4, 4.4, 5.5, 5.1, 5.1, 3.2, 3.9, 3.2, 4.9, 4.3, 4.8, 2.4, 5.5, 5.5, 3.7};
+
+        // Add values to A sketch
+        for (int i = 0; i < groupA.length; i++) {
+            sketchA.update(i, new double[] {groupA[i]});
+        }
+
+        // Add values to B sketch
+        for (int i = 0; i < groupB.length; i++) {
+            sketchB.update(i, new double[] {groupB[i]});
+        }
+
+        // Convert to a tuple and execute the UDF
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketchA.compact().toByteArray()),
+                                                  new DataByteArray(sketchB.compact().toByteArray()));
+        Tuple resultTuple = func.exec(inputTuple);
+
+        // Should get 2 p-values back
+        Assert.assertNotNull(resultTuple);
+        Assert.assertEquals(resultTuple.size(), 1);
+
+        // Check p-value values, with a delta
+        Assert.assertEquals((double) resultTuple.get(0), 0.0043, 0.0001);
+    }
+
+    /**
+     * Check p-value for a large data set.
+     */
+    @Test
+    public void largeDataSet() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        // Create the two sketches
+        ArrayOfDoublesUpdatableSketch sketchA = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(4096)
+                                                    .build();
+        ArrayOfDoublesUpdatableSketch sketchB = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(4096)
+                                                    .build();
+
+        // Number of values to use.
+        int n = 1000000;
+        int bShift = 1000;
+        double[] a = new double[n];
+        double[] b = new double[n];
+
+        // Add values to A sketch
+        for (int i = 0; i < n; i++) {
+            sketchA.update(i, new double[] {i});
+            a[i] = i;
+        }
+
+        // Add values to B sketch
+        for (int i = 0; i < n; i++) {
+            int val = i + bShift;
+            sketchB.update(i, new double[] {val});
+            b[i] = val;
+        }
+
+        double meanA = StatUtils.mean(a);
+        double meanB = StatUtils.mean(b);
+        double varianceA = StatUtils.variance(a);
+        double varianceB = StatUtils.variance(b);
+        double numA = a.length;
+        double numB = b.length;
+
+        TTest tTest = new TTest();
+        double expectedPValue = tTest.tTest(a, b);
+
+        // Convert to a tuple and execute the UDF
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketchA.compact().toByteArray()),
+                                                  new DataByteArray(sketchB.compact().toByteArray()));
+        Tuple resultTuple = func.exec(inputTuple);
+
+        // Should get 2 p-values back
+        Assert.assertNotNull(resultTuple);
+        Assert.assertEquals(resultTuple.size(), 1);
+
+        // Check p-value values, with a delta
+        Assert.assertEquals((double) resultTuple.get(0), expectedPValue, 0.01);
+    }
+
+    /**
+     * Check p-value for two metrics at the same time.
+     */
+    @Test
+    public void twoMetrics() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        // Create the two sketches
+        ArrayOfDoublesUpdatableSketch sketchA = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(2)
+                                                    .setNominalEntries(128)
+                                                    .build();
+        ArrayOfDoublesUpdatableSketch sketchB = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(2)
+                                                    .setNominalEntries(128)
+                                                    .build();
+
+        // Sample dataset (smoker/non-smoker brain size)
+        double[] groupA = {7.3, 6.5, 5.2, 6.3, 7.0, 5.9, 5.2, 5.0, 4.7, 5.7, 5.7, 3.3, 5.0, 4.6, 4.8, 3.8, 4.6};
+        double[] groupB = {4.2, 4.0, 2.6, 4.9, 4.4, 4.4, 5.5, 5.1, 5.1, 3.2, 3.9, 3.2, 4.9, 4.3, 4.8, 2.4, 5.5, 5.5, 3.7};
+
+        // Add values to A sketch
+        for (int i = 0; i < groupA.length; i++) {
+            sketchA.update(i, new double[] {groupA[i], i});
+        }
+
+        // Add values to B sketch
+        for (int i = 0; i < groupB.length; i++) {
+            sketchB.update(i, new double[] {groupB[i], i});
+        }
+
+        // Convert to a tuple and execute the UDF
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketchA.compact().toByteArray()),
+                                                  new DataByteArray(sketchB.compact().toByteArray()));
+        Tuple resultTuple = func.exec(inputTuple);
+
+        // Should get 2 p-values back
+        Assert.assertNotNull(resultTuple);
+        Assert.assertEquals(resultTuple.size(), 2);
+
+        // Check expected p-value values, with a delta
+        Assert.assertEquals((double) resultTuple.get(0), 0.0043, 0.0001);
+        Assert.assertEquals((double) resultTuple.get(1), 0.58, 0.01);
+    }
+
+    /**
+     * Check with sketch having only one input.
+     */
+    @Test
+    public void sketchWithSingleValue() throws Exception {
+        EvalFunc<Tuple> func = new ArrayOfDoublesSketchesToPValueEstimates();
+
+        // Create the two sketches
+        ArrayOfDoublesUpdatableSketch sketchA = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(128)
+                                                    .build();
+        ArrayOfDoublesUpdatableSketch sketchB = new ArrayOfDoublesUpdatableSketchBuilder()
+                                                    .setNumberOfValues(1)
+                                                    .setNominalEntries(128)
+                                                    .build();
+
+        // Sample dataset
+        double[] groupA = {7.3, 6.5, 5.2, 6.3, 7.0, 5.9, 5.2, 5.0, 4.7, 5.7, 5.7, 3.3, 5.0, 4.6, 4.8, 3.8, 4.6};
+        double[] groupB = {5.0};
+
+        // Add values to A sketch
+        for (int i = 0; i < groupA.length; i++) {
+            sketchA.update(i, new double[] {groupA[i]});
+        }
+
+        // Add values to B sketch
+        for (int i = 0; i < groupB.length; i++) {
+            sketchB.update(i, new double[] {groupB[i]});
+        }
+
+        // Convert to a tuple and execute the UDF
+        Tuple inputTuple = PigUtil.objectsToTuple(new DataByteArray(sketchA.compact().toByteArray()),
+                                                  new DataByteArray(sketchB.compact().toByteArray()));
+        Tuple resultTuple = func.exec(inputTuple);
+
+        // Should get null back, as one of the sketches had fewer than 2 items
+        Assert.assertNull(resultTuple);
+    }
+}
